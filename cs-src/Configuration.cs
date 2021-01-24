61e0d2b65dd29d669bbc2270c617ef976f16130e
DSikes <me@dsikes.com>
2021-01-24 05:24:07 -0500

    Added and set up ArgumentWorker.cs, which handles checking and getting data from args.

---
+++ cs-src/Configuration.cs
@@ -0,0 +1,8 @@
+namespace cs_src
+{
+    public static class Configuration
+    {
+        // This should be a json file as well, but if you set it up like this, it's easy to convert later
+        public static string PathArgString => "/path";
+    }
+}
--- cs-src/Program.cs
+++ cs-src/Program.cs
@@ -6,8 +6,38 @@ namespace cs_src
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-            Console.ReadLine();
+            Console.WriteLine("Welcome.");
+
+            // I could make all the methods static but it's easier to do other things later.
+            var argWorker = new ArgumentWorker(args);
+            
+            if (argWorker.CheckForPathArg())
+            {
+                // PS C:\Users\David\Documents\GitHub\config-loader\cs-src\bin\Debug> .\cs_src.exe /path "C:\Users\David\Inventor Configs\app_config.json"
+                // Welcome.
+                //
+                // Args length:
+                // 2
+                //
+                // Arg 0 is: /path
+                // Arg 1 is: C:\Users\David\Inventor Configs\app_config.json
+                //
+                // Could not find app_config.json in same directory as this application.
+                //
+                // Please rerun with /path and the path to the JSON file.
+                //
+                // Press enter to exit...
+                
+            } 
+            // else if (FoundJson())
+            // {
+            //     // Since it's an elseif, args will override looking for json locally
+            //     
+            // }
+            else
+            {
+                argWorker.PromptUser();
+            }
         }
     }
 }
