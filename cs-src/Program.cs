073f9853ce355ca575c68d56e93f0a73f0ddda41
DSikes <me@dsikes.com>
2021-01-24 04:46:53 -0500

    Added C# project

---
+++ cs-src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cs_src
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
