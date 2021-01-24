76147b38a430b761087a79a82e139627e8043070
jordanrobot <119146+jordanrobot@users.noreply.github.com>
2021-01-24 02:12:16 -0500

    First pass

---
+++ src/Header.vb
@@ -0,0 +1,11 @@
+﻿'</CollectorHeader>
+
+#Region "Imports" '<CollectorPrepend>'</CollectorPrepend>
+
+AddReference "Newtonsoft.Json.dll"
+
+Imports System.IO
+Imports System.Runtime.InteropServices
+Imports Autodesk.Inventor.Interop
+
+#End Region '<CollectorPrepend>'</CollectorPrepend>
